f10b204eba067643c1de0edd779ebece64945636
Clif Wolfe <clifwolfe1@gmail.com>
2023-12-17 17:18:45 -0500

    more day 8

--- day8/solve.py
+++ day8/solve.py
@@ -3,7 +3,7 @@ import re
 from aoc23_utils import cli, log, BinaryTreeDir, BinaryTreeNode
 import logging
 import enum
-from collections import deque
+from collections import deque, defaultdict
 
 
 class NamedBinaryTreeNode(BinaryTreeNode):
@@ -55,15 +55,39 @@ end_node = node_lookup['ZZZ']
 
 p1_count = binary_tree_follow_instr_to_node(start_node, end_node, instruction_list)
 print("part 1 %d" % p1_count)
+initial_instruction_list = instruction_list.copy()
 
 start_nodes = [i for i in node_lookup.values() if i.name.endswith("A")]
 
-p2_count = 0
-node = start_node
-current_nodes = start_nodes.copy()
-while not all([n.name.endswith("Z") for n in current_nodes]):
-    current_nodes = [n.get_child(instruction_list[0]) for n in current_nodes]
-    p2_count += 1
-    instruction_list.rotate(-1)
+loop_counts = {}
+visit_lists = defaultdict(list)
 
-print("part 2 %d" % p2_count)
+for node in start_nodes:
+    instruction_list = initial_instruction_list
+    initial_node = node
+    count = 0
+    found_loop = False
+    log.debug("starting node %s", node.name)
+    while not found_loop:
+        node = node.get_child(instruction_list[0])
+        visit_lists[initial_node.name].append(node)
+        count += 1
+        instruction_list.rotate(-1)
+        if node.name.endswith("Z") and count != 0:
+            log.debug("found loop for %s to %s at count %d", initial_node.name, node.name, count)
+            found_loop = True
+    loop_counts[initial_node.name] = count
+
+
+
+
+# really inefficient
+# p2_count = 0
+# node = start_node
+# current_nodes = start_nodes.copy()
+# while not all([n.name.endswith("Z") for n in current_nodes]):
+#     current_nodes = [n.get_child(instruction_list[0]) for n in current_nodes]
+#     p2_count += 1
+#     instruction_list.rotate(-1)
+#
+# print("part 2 %d" % p2_count)
